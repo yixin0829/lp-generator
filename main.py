5c988080c4e2a4341ff29694b90b58ac05bb1a12
yixin0829 <yixin0829@outlook.com>
2022-12-30 23:08:03 -0600

    added user input content moderation and max input length limit

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ class LearningPath(BaseModel):
     usage: dict
     status_code: int
 
+class HTTPError(BaseModel):
+    detail: str
+
+    class Config:
+        schema_extra = {
+            "example": {"detail": "HTTPException raised."},
+        }
+
 @app.get("/")
 def get_root():
     return {
@@ -38,31 +46,53 @@ def get_root():
     }
 
 
-@app.get("/v1/lp/{topic}", response_model=LearningPath)
+@app.get(
+    "/v1/lp/{topic}",
+    responses={
+        status.HTTP_200_OK: {"model": LearningPath},
+        status.HTTP_400_BAD_REQUEST: {"model": HTTPError},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError},
+        })
 def get_lp(topic:str):
     """Take any topic and call OpenAI's Completion engpoint to generate a learning path in JSON string format"""
 
     try:
         ######### Code for production (fetching from API)  ###########
+        # Enforce client input length to prevent prompt injection
+        if len(topic) > 60:
+            return HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Input path parameter exceeds maximum length allowed (60 characters)."
+            )
+        
+        # Enforce client content moderation
+        mod_response = openai.Moderation.create(input=topic)
+        content_flag = mod_response.get("results")[0].get("flagged")
+        if content_flag:
+            return HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User input does not complies with OpenAI's content policy. https://beta.openai.com/docs/usage-policies/content-policy"
+            )
+
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(topic),
-            max_tokens = 300,
+            max_tokens = 200,
             temperature = 1
         )
         return {
             "topic": topic,
             "completion": json.loads(response.choices[0].text),
             "usage": response.usage,
-            "status_code": 200
+            "status_code": status.HTTP_200_OK
         }
         ##############################################################
 
         ########## Code for testing FE (fetching from json) ##########
         # with open("./mock_response.json", "r") as f:
         #     response = json.load(f)
+        #     return response
         ##############################################################
 
-        return response
     except Exception as error:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=error)
