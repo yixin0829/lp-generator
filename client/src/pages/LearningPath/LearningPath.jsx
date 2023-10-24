53a29c7b45c51ac502229f9390eaf9bd6e36d60d
yixin0829 <yixin0829@outlook.com>
2023-10-23 21:11:30 -0400

    switch to prod endpoint

--- client/src/pages/LearningPath/LearningPath.jsx
+++ client/src/pages/LearningPath/LearningPath.jsx
@@ -31,10 +31,10 @@ import { useSnackbar } from "notistack";
 
 async function generateLp(topic) {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/v2/lp/${topic}`);
-    // const response = await fetch(
-    //   `http://lp-app-load-balancer-1350610118.us-east-2.elb.amazonaws.com/v2/lp/${topic}`
-    // );
+    // const response = await fetch(`http://127.0.0.1:8000/v2/lp/${topic}`);
+    const response = await fetch(
+      `http://lp-app-load-balancer-1350610118.us-east-2.elb.amazonaws.com/v2/lp/${topic}`
+    );
     const data = await response.json();
     return [data, response.status];
   } catch (error) {
